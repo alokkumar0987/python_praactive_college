8f8d49378248d5f370447fce4080aff4b1b6c79e
alokkumar0987 <alok33778@gmail.com>
2025-07-29 20:25:46 +0530

    print_statement

---
+++ python_practice/Day_00/p_04.py
@@ -0,0 +1,8 @@
+a=100
+b=500
+print("value is", a,b,sep=":",end=".........")
+print("\n")
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+c=a+b
+print("Sum of", a, "and", b, "is:", a + b)
