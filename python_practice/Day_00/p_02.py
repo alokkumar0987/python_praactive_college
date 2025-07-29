2f52adffa39064b591e056ea2f530d2b75c4e06b
alokkumar0987 <alok33778@gmail.com>
2025-07-29 19:42:06 +0530

    input function_with operation

---
+++ python_practice/Day_00/p_02.py
@@ -0,0 +1,11 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+print("Sum of", a, "and", b, "is:", a + b)
+print("Difference of", a, "and", b, "is:", a - b)
+print("Product of", a, "and", b, "is:", a * b) 
+print("Quotient of", a, "and", b, "is:", a / b)
+print("Remainder of", a, "and", b, "is:", a % b)  
+
+print("Floor division of", a, "and", b, "is:", a // b)
+print("Exponentiation of", a, "and", b, "is:", a ** b)
+
